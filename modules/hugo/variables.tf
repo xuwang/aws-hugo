4e87efb50616f27cc2081e883d5b108c6cbc354b
Xu Wang <xuwang@gmail.com>
2016-03-09 15:37:02 -0800

    add sleep time on policy attachement

--- modules/hugo/variables.tf
+++ modules/hugo/variables.tf
@@ -15,7 +15,7 @@ variable "www_fqdn" {
     default = "www.example.com"
 }
 
-# www bucket policy template, $${bucket_name} should be supplied by caller.
+# www bucket policy template, ${bucket_name} should be supplied by caller.
 variable "html_policy_tmpl" {
     default = "artifacts/policies/html-bucket-policy.tmpl"
 }
@@ -26,5 +26,7 @@ variable "lambda_role_policy_tmpl" {
 
 variable "lambda_function_tar_url" {
     # See section 5 of http://bezdelev.com/post/hugo-aws-lambda-static-website/
+    #   Download link: Download Lambda function for Hugo
+    #   MD5 checksum: 030c8b8432d069c734d968f3b150ed73
     default = "https://drive.google.com/uc?export=download&id=0ByJc_EZ6U7G8NFE5QmV3UzcwcWs"
 }
