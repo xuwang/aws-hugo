4a1d7d95273f94f25627c36ea97f9c182ba33b37
Xu Wang <xuwang@gmail.com>
2016-03-09 22:35:06 -0800

    terraform output needs --module=hugo.

--- terraforms/s3-event-source.sh
+++ terraforms/s3-event-source.sh
@@ -1,14 +1,15 @@
-#!/bin/evn bash
+#!/usr/bin/env bash
 
 # Terraform has yet to add this, but for now we have to use awscli to accomplish setting lambda s3 triggers
 
+source_bucket_id=$(terraform output -module=hugo source_bucket_id)
+hugo_lambda_arn=$(terraform output -module=hugo hugo_lambda_arn)
+aws s3api put-bucket-notification-configuration \
+        --bucket "$source_bucket_id" \
+        --notification-configuration \
+'{"LambdaFunctionConfigurations": [{"LambdaFunctionArn": "$hugo_lambda_arn", "Events": ["s3:ObjectCreated:*"], "Filter": {"Key": {"FilterRules": [{"Name": "prefix", "Value": "targets"}]}}}]}'
 
 aws s3api put-bucket-notification-configuration \
-        --bucket "`terraform output source_bucket_id`" \
+        --bucket "$source_bucket_id" \
         --notification-configuration \
-'{"LambdaFunctionConfigurations": [{"LambdaFunctionArn": "'`terraform output hugo_lambda_arn`'", "Events": ["s3:ObjectCreated:*"], "Filter": {"Key": {"FilterRules": [{"Name": "prefix", "Value": "targets"}]}}}]}'
-
-aws s3api put-bucket-notification-configuration \
-        --bucket "`terraform output source_bucket_id`" \
-        --notification-configuration \
-'{"LambdaFunctionConfigurations": [{"LambdaFunctionArn": "'`terraform output hugo_lambda_arn`'", "Events": ["s3:Object Removed:*"], "Filter": {"Key": {"FilterRules": [{"Name": "prefix", "Value": "targets"}]}}}]}'
+'{"LambdaFunctionConfigurations": [{"LambdaFunctionArn": "$hugo_lambda_arn", "Events": ["s3:Object Removed:*"], "Filter": {"Key": {"FilterRules": [{"Name": "prefix", "Value": "targets"}]}}}]}'
