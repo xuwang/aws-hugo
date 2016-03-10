e167f7e54623726f55a479abbe3b3fbc2686d30a
Xu Wang <xuwang@gmail.com>
2016-03-09 17:08:37 -0800

    fix lambda_function_url

--- modules/hugo/variables.tf
+++ modules/hugo/variables.tf
@@ -25,8 +25,7 @@ variable "lambda_role_policy_tmpl" {
 }
 
 variable "lambda_function_tar_url" {
-    # See section 5 of http://bezdelev.com/post/hugo-aws-lambda-static-website/
     #   Download link: Download Lambda function for Hugo
     #   MD5 checksum: 030c8b8432d069c734d968f3b150ed73
-    default = "https://drive.google.com/uc?export=download&id=0ByJc_EZ6U7G8NFE5QmV3UzcwcWs"
+    default = "https://github.com/xuwang/aws-hugo/files/166363/hugo_aws_lambda_20151129.zip"
 }
