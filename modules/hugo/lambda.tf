b47c5685bba830396be49ce074a3daa121d101f5
Xu Wang <xuwang@gmail.com>
2016-03-09 16:50:58 -0800

    fix the var.prefix in lambda.tf

--- modules/hugo/lambda.tf
+++ modules/hugo/lambda.tf
@@ -1,9 +1,9 @@
 # Lambda function to call hugo
 
 resource "aws_lambda_function" "hugo_lambda" {
-    depends_on = ["template_file.lambda_download"]
-    filename = "artifacts/${prefix}-lambda-function.zip"
-    function_name = "${prefix}-lambda"
+    depends_on = ["null_resource.lambda_download"]
+    filename = "artifacts/${var.prefix}-lambda-function.zip"
+    function_name = "${var.prefix}-lambda"
     role = "${aws_iam_role.lambda_role.arn}"
     handler = "RunHugo.handler"
     runtime = "nodejs"
@@ -12,10 +12,21 @@ resource "aws_lambda_function" "hugo_lambda" {
 }
 
 # Download hugo lambda function
-resource "template_file" "lambda_download" {
-    template = "/dev/null"
+resource "null_resource" "lambda_download" {
+
+    triggers {
+        lambda_function_tar_url = "${var.lambda_function_tar_url}"
+    }
 
     provisioner "local-exec" {
-        command = "curl -s -o artifacts/${prefix}-lambda-function.zip ${var.lambda_function_tar_url}"
+        command = "curl -s -o artifacts/${var.prefix}-lambda-function.zip ${var.lambda_function_tar_url}"
     }
+}
+
+# Not sure about event_source_arn is ok
+resource "aws_lambda_event_source_mapping" "event_source_mapping" {
+    event_source_arn = "${aws_lambda_function.hugo_labda.arn}"
+    function_name = "${aws_lambda_function.hugo_labda.arn}"
+    enabled = true
+    starting_position = "TRIM_HORIZON|LATEST"
 }
