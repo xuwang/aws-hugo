b2c0b46363c291738705d44aed064569bc764261
Xu Wang <xuwang@gmail.com>
2016-03-10 21:42:59 -0800

    fix profile name for aws call.

--- terraforms/delete-s3-trigger.sh
+++ terraforms/delete-s3-trigger.sh
@@ -1,10 +1,8 @@
-#!/bin/evn bash
+#!/usr/bin/env bash
 
 # Terraform has yet to add this, but for now we have to use awscli to accomplish removing lambda s3 triggers
+input_bucket_id=$(terraform output -module=hugo input_bucket_id)
 
-source_bucket_id=$(terraform output -module=hugo source_bucket_id)
-hugo_lambda_arn=$(terraform output -module=hugo hugo_lambda_arn)
-
-aws s3api put-bucket-notification-configuration \
-        --bucket "$source_bucket_id" \
-        --notification-configuration '{}'
+aws --profile myhugo s3api put-bucket-notification-configuration \
+        --bucket "$input_bucket_id" \
+        --notification-configuration="{}"
