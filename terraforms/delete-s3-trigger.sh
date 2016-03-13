4a22e85b17828e2278c987dc53b657abfdfd906e
Xu Wang <xuwang@gmail.com>
2016-03-12 20:55:47 -0800

    delete  AllowLambdaInvokeFunction permission.

--- terraforms/delete-s3-trigger.sh
+++ terraforms/delete-s3-trigger.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 # Terraform has yet to add this, but for now we have to use awscli to accomplish removing lambda s3 triggers
+profile=myhugo
 input_bucket_id=$(terraform output -module=hugo input_bucket_id)
+hugo_lambda_name="$(terraform output -module=hugo hugo_lambda_name)"
 
-aws --profile myhugo s3api put-bucket-notification-configuration \
+aws --profile $profile lambda remove-permission --function-name $hugo_lambda_name \
+    --statement-id AllowLambdaInvokeFunction
+aws --profile $profile s3api put-bucket-notification-configuration \
         --bucket "$input_bucket_id" \
         --notification-configuration="{}"
