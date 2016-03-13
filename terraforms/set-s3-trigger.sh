3d5cc3c95eddecb408529028135f2debd514ac6b
Xu Wang <xuwang@gmail.com>
2016-03-12 20:57:28 -0800

    add invoke permission to s3 bucket.

--- terraforms/set-s3-trigger.sh
+++ terraforms/set-s3-trigger.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 # Terraform has yet to add this, but for now we have to use awscli to accomplish setting lambda s3 triggers
-
+profile='myhugo'
 input_bucket_id=$(terraform output -module=hugo input_bucket_id)
 hugo_lambda_name="$(terraform output -module=hugo hugo_lambda_name)"
 hugo_lambda_arn=$(aws --profile myhugo lambda  list-functions \
     | jq -r ".Functions | .[] | select(.FunctionName==\"$hugo_lambda_name\") \
     | .FunctionArn")
+
+aws --profile $profile lambda add-permission --function-name $hugo_lambda_name --statement-id AllowLambdaInvokeFunction \
+    --action "lambda:InvokeFunction" --principal s3.amazonaws.com --source-arn "arn:aws:s3:::$input_bucket_id" 
 
 read -r -d '' EVENT << EOF
 {
